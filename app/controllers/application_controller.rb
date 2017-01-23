f0c67a73cd12a3719a6de44c62f3fe40f2d14375
chaneyventura <chaneyventura@gmail.com>
2017-01-23 14:22:59 -0700

    Update call to rubygems for copyright generator

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,9 @@ class ApplicationController < ActionController::Base
   include SetSource
   include CurrentUserConcern
   include DefaultPageContent
+
+  before_action :set_copyright
+  def set_copyright
+    @copyright = VenturaViewTool::Renderer.copyright 'Chaney Ventura', 'All rights reserved'
+  end
 end
