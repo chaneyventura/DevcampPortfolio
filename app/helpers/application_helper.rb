8125e79963374b7184ea35e849ecba0bbf506f00
chaneyventura <chaneyventura@gmail.com>
2017-01-25 10:58:29 -0700

    Implement partials for blog, font awesome, and pagination

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,13 +6,17 @@ module ApplicationHelper
       (link_to "Login", new_user_session_path, class: style)
   else
       link_to "Logout", destroy_user_session_path, method: :delete, class: style
-    end
   end
+end
 
   def source_helper(layout_name)
       if session[:source]
         greeting = "Thanks for visiting me from #{session[:source]} and you are on the #{layout_name} layout."
         content_tag(:p, greeting, class: "source-greeting")
       end
-   end 
+   end
+
+    def copyright_generator
+    VenturaViewTool::Renderer.copyright 'Chaney Ventura', 'All rights reserved'
+  end 
 end
