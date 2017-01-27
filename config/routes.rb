f5d50c4a8a128fddea17fcddd3e7fbdbcd82215c
chaneyventura <chaneyventura@gmail.com>
2017-01-27 15:24:09 -0700

    refactored, added missing code, edited routes, bugs, and root erros

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,26 @@ Rails.application.routes.draw do
   end
 
   resources :portfolios, except: [:show]
-  get 'portfolio/:id', to: 'portfolios#show', as: 'show_portfolio'
+  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
   get 'angular-items', to: 'portfolios#angular'
 
 end
+
+# Rails.application.routes.draw do
+#   devise_for :users, path: "", path_names: {sign_in: "login", sign_out: "logout", sign_up: "register"}
+#   resources :portfolios, except: [:show]
+#   get "portfolio/:id", to: "portfolios#show", as: "portfolio_show"
+#   get "angular-items", to: "portfolios#angular"
+ 
+#   get 'about', to: 'pages#about'
+#   get 'contact', to: 'pages#contact'
+
+#   resources :blogs do
+#     member do
+#       get :toggle_status
+#     end
+#   end
+
+#   root to: 'pages#home'
+ 
+# end
