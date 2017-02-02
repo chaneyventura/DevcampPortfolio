bc1698f5fcb58bad31a631fba4bd216cbf3acc7c
chaneyventura <chaneyventura@gmail.com>
2017-02-02 15:39:35 -0700

    Fix routing errors and implement action cable for comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,14 @@
 Rails.application.routes.draw do
-  resources :comments
-  devise_for :users, path: '', path_names: {sign_in: 'login', sign_out: 'logout', sign_up: 'register'}
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
   resources :portfolios, except: [:show] do
     put :sort, on: :collection
   end
-
-   root to: 'pages#home'
-
-  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
   get 'angular-items', to: 'portfolios#angular'
+  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
 
-  get 'about', to: 'pages#about'
+  get 'about-me', to: 'pages#about'
   get 'contact', to: 'pages#contact'
-   get 'tech-news', to: 'pages#tech_news'
-
+  get 'tech-news', to: 'pages#tech_news'
 
   resources :blogs do
     member do
@@ -23,25 +18,5 @@ Rails.application.routes.draw do
 
   mount ActionCable.server => '/cable'
 
-
+  root to: 'pages#home'
 end
-
-# Rails.application.routes.draw do
-#  resources :comments
-#   devise_for :users, path: "", path_names: {sign_in: "login", sign_out: "logout", sign_up: "register"}
-#   resources :portfolios, except: [:show]
-#   get "portfolio/:id", to: "portfolios#show", as: "portfolio_show"
-#   get "angular-items", to: "portfolios#angular"
- 
-#   get 'about', to: 'pages#about'
-#   get 'contact', to: 'pages#contact'
-
-#   resources :blogs do
-#     member do
-#       get :toggle_status
-#     end
-#   end
-
-#   root to: 'pages#home'
- 
-# end
