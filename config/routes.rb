f76ab9e670b0d02a96495d9642b6d99514c3007e
chaneyventura <chaneyventura@gmail.com>
2017-02-02 14:11:02 -0700

    Create a comment broadcast, configure the websocket route, update the comment model with a callback

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,9 @@ Rails.application.routes.draw do
 
    root to: 'pages#home'
 
+  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
+  get 'angular-items', to: 'portfolios#angular'
+
   get 'about', to: 'pages#about'
   get 'contact', to: 'pages#contact'
    get 'tech-news', to: 'pages#tech_news'
@@ -18,9 +21,8 @@ Rails.application.routes.draw do
     end
   end
 
+  mount ActionCable.server => '/cable'
 
-  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
-  get 'angular-items', to: 'portfolios#angular'
 
 end
 
